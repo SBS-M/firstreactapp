07de7faa02d9842a125e73e8fdeac7960c2f8272
Sebastian Balanta Salazar <sebastian.balanta@novaip.com.co>
2019-08-30 17:44:14 -0500

    Primer commit de tutorial de react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
+
+/* function Helloworld (props) {
+  console.log(props);
+  return (
+    <div id="hello">
+    <h3>{props.subtitle}</h3>
+    {props.pruebaText}
+    </div>
+  );
+} */
+
+/* const App = () => <div>This is my component const -> <Helloworld/> </div> */
+
+/* class App extends React.Component{
+  render(){
+    return <div>This is my component class: <Helloworld/></div>
+  } 
+} */
+
+class Helloworld extends React.Component {
+
+  state = { 
+    show: false
+  }
+
+  render(){
+    if(this.state.show){
+      return (
+        <div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.pruebaText}
+        <button onClick={() => alert('welcome')}>Toggle show</button>
+        </div>
+      );
+    }else{
+      return <h1>No hay elementos</h1>
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>This is my component function: 
+      <Helloworld subtitle="Titulo SuperGG" pruebaText="Prueba 1"/> 
+      <Helloworld pruebaText="Prueba 2" /> 
+      <Helloworld pruebaText="Prueba 3"/></div>
   );
 }
 
