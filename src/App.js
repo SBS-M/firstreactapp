0b60579cb6dc27731cc8a29f254820ba646583aa
Sebastian Balanta Salazar <sebastian.balanta@novaip.com.co>
2019-09-03 23:10:53 -0500

    Segunda parte cubierta de prop (inputs y outputs), estados, ahora se esta interactuando con otros elementos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
-import React from 'react';
-/* import logo from './logo.svg'; */
+import React, { Component } from 'react';
 import './App.css';
+import tasks from '../src/sample/task.json';
+
+console.log("tasks ", tasks);
+
+class App extends Component {
+
+  state = {
+    tasks: tasks
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.tasks.map((e) =>
+          <p>
+            {e.id} - {e.title} - {e.descripcion}
+          </p>)}
+      </div>
+    )
+  }
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
 
 /* function Helloworld (props) {
   console.log(props);
@@ -17,12 +48,12 @@ import './App.css';
 /* class App extends React.Component{
   render(){
     return <div>This is my component class: <Helloworld/></div>
-  } 
+  }
 } */
 
-class Helloworld extends React.Component {
+/* class Helloworld extends React.Component {
 
-  state = { 
+  state = {
     show: false
   }
 
@@ -41,13 +72,46 @@ class Helloworld extends React.Component {
   }
 }
 
-function App() {
-  return (
-    <div>This is my component function: 
-      <Helloworld subtitle="Titulo SuperGG" pruebaText="Prueba 1"/> 
-      <Helloworld pruebaText="Prueba 2" /> 
-      <Helloworld pruebaText="Prueba 3"/></div>
-  );
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    console.log("this.state.show ", this.state.show);
+
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.pruebaText}
+          <button onClick={this.toggleShow}>Toogle Show</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>No hay elementos.</h1>
+          <button onClick={this.toggleShow}>Toggle revert</button>
+
+        </div>)
+
+
+    }
+
+  }
 }
 
-export default App;
+function App() {
+  return (
+    <div>This is my component function:
+      <Helloworld subtitle="Titulo SuperGG" pruebaText="Prueba 1" />
+      <Helloworld pruebaText="Prueba 2" />
+      <Helloworld pruebaText="Prueba 3" /></div>
+  );
+} */
